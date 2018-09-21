64513b339ec2551212eef7119d1adfcbdeb12325
Kyle Throssell <36345062+ktthross@users.noreply.github.com>
2018-09-21 00:54:33 -0400

    Nothing crazy
    Just making progress on fileio between parts of the code.  HFB is coming once I figure out the pieces.  Just trying to do a little each day

--- sladet.cpp
+++ sladet.cpp
@@ -4,21 +4,45 @@
 #include <iostream>
 #include "qtzio.h"
 #include "sladet.h"
+#include <sys/stat.h>
+
+/**
+ * Check if a file exists
+ * @return true if and only if the file exists, false else
+ */
+bool fileExists(const std::string& file) {
+
 
 const std::string wfnIO = "qtz.wfn.bin" ;
 
 template<typename s, int r, int c>
-void write_to_bin( sladet< s, r, c>& w){ 
-/* Write the wfntype, MO coefficients and eigenvalues to an external file. */
+void save_slater_det( sladet< s, r, c>& w, int& cntl){
+/* A controlling routine to know where to create a new external file
+   or to append to an existing file.
+
+  cntl : 
+    0(default) - write to a new file or overwrite one that already exists */
   std::ofstream F_OUT ;
-  
-  try {
-    F_OUT.open( wfnIO, std::ofstream::binary | std::ofstream::out) ;
-  } catch ( std::fstream::failure& e) {
-    std::cout << e.what() << std::endl ;
-  } catch (...) {
-    std::cout << " Default excpetion inside write_to_bin " << std::endl ;
-    }
+  struct stat buf;
+
+  /* Check if the file exists. */
+  if ( stat(wfnIO.c_str(), &buf) == 0) {
+
+    try {
+      F_OUT.open( wfnIO, std::ofstream::binary | std::ofstream::out) ;
+    } catch ( std::fstream::failure& e) {
+      std::cout << e.what() << std::endl ;
+    } catch (...) {
+      std::cout << " Default excepetion inside write_to_bin " << std::endl ;
+      }
+
+  return ;
+
+}
+
+template<typename s, int r, int c>
+void write_to_bin( sladet< s, r, c>& w, std::ofstream& F_OUT){ 
+/* Write the wfntype, MO coefficients and eigenvalues to an external file. */
 
   F_OUT.write( s.wfntp) ; F_OUT.write( s.e_scf) ;
   write_eigen_bin( s.moc, F_OUT) ; write_eigen_bin( s.eig, F_OUT) ;
@@ -32,8 +56,8 @@ void write_to_bin( sladet< s, r, c>& w){
 template void write_to_bin( sladet< std::complex<double>, Eigen::Dynamic, Eigen::Dynamic>&) ;
 
 template<typename s, int r, int c>
-void load_sd( sladet< s, r, c>& w) {
-/* Write the wfntype, MO coefficients and eigenvalues to an external file. */
+void load_slater_det( sladet< s, r, c>& w) {
+/* */
   std::ofstream F_IN ;
 
   try {
@@ -41,7 +65,7 @@ void load_sd( sladet< s, r, c>& w) {
   } catch ( std::fstream::failure& e) {
     std::cout << e.what() << std::endl ;
   } catch (...) {
-    std::cout << " Default excpetion inside load_sd " << std::endl ;
+    std::cout << " Default excepetion inside load_sd " << std::endl ;
     }
 
   F_IN.read( s.e_scf) ;
--- sladet.h
+++ sladet.h
@@ -16,7 +16,13 @@ struct sladet {
 
 } ;
 
+void save_slater_det( sladet< s, r, c>& w, int& cntl) ;
+
 template<typename s, int r, int c>
-void write_to_bin( sladet<s, r, c>& w) ;
+void write_to_bin( sladet< s, r, c>& w, std::ofstream& F_OUT) ;
+
+void load_slater_det( sladet< s, r, c>& w) ;
+
+void read_from_bin( sladet< s, r, c>& w, std::ifstream& F_IN) ;
 
 #endif
